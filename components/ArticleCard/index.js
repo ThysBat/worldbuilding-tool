75e4ad378b541a12dde0a38c107c6a9573a96b39
David Illies <dav.illies@web.de>
2023-07-07 12:29:25 +0200

    add toggle to article

--- components/ArticleCard/index.js
+++ components/ArticleCard/index.js
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import styled from "styled-components";
 import Card from "../Card";
+import Button from "../Button";
 import DeleteButton from "../DeleteButton";
+
+import { ArrowUpIcon } from "../../assets/icons";
+import { ArrowDownIcon } from "../../assets/icons";
 
 export default function ArticleCard({
   article,
   handleUpdate,
   handleDeleteArticle,
 }) {
+  const contentRef = useRef(null);
   const [title, setTitle] = useState(article.title);
   const [content, setContent] = useState(article.content);
+  const [expand, setExpand] = useState(false);
+
+  useEffect(() => {
+    if (contentRef && expand) {
+      // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+      contentRef.current.style.height = "0px";
+      const scrollHeight = contentRef.current.scrollHeight;
+
+      // We then set the height directly, outside of the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      contentRef.current.style.height = scrollHeight + "px";
+    } else {
+      contentRef.current.style.height = "unset";
+      contentRef.current.rows = 3;
+    }
+  }, [contentRef, content, expand]);
 
   function handleTitleChange(event) {
     setTitle(event.target.value);
@@ -25,23 +46,36 @@ export default function ArticleCard({
     handleUpdate(article.id, "content", content);
   }
 
+  function toggleArticle() {
+    setExpand(!expand);
+  }
+
   return (
     <Article as="article">
-      <StyledInput
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={handleTitleChange}
-        onBlur={handleTitleBlur}
-      />
+      <TitleWrapper>
+        <StyledInput
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={handleTitleChange}
+          onBlur={handleTitleBlur}
+        />
+        <ToggleButton onClick={toggleArticle}>
+          {expand ? (
+            <ArrowUpIcon height={36} fill="var(--primary)" />
+          ) : (
+            <ArrowDownIcon height={36} fill="var(--primary)" />
+          )}
+        </ToggleButton>
+      </TitleWrapper>
       <StyledInput
         as="textarea"
-        rows={4}
+        ref={contentRef}
         placeholder="Content"
         value={content}
         onChange={handleContentChange}
         onBlur={handleContentBlur}
-      />
+      ></StyledInput>
       <DeleteButton
         variant="transparent"
         handleDelete={() => handleDeleteArticle(article.id)}
@@ -53,6 +87,15 @@ export default function ArticleCard({
     </Article>
   );
 }
+
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const ToggleButton = styled(Button)`
+  flex: 1;
+`;
 
 const Article = styled(Card)`
   flex-direction: column;
